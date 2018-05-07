62e325ebfab318477e7249b7e9dfb1aee9153cf6
OlehFliurkevych <oleh0809@gmail.com>
2018-05-08 00:16:41 +0300

    Add relationships

--- src/main/java/com/botscrew/entity/DepartmentEntity.java
+++ src/main/java/com/botscrew/entity/DepartmentEntity.java
@@ -2,7 +2,11 @@ package com.botscrew.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
 
 @Entity
 @Table(name="department")
@@ -14,5 +18,47 @@ public class DepartmentEntity extends BaseEntity{
 	@Column(name="head_of_department_name")
 	private String headOfDepartmentName;
 	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="lector_id")
+	private LectorEntity lector;
+
+	public DepartmentEntity(String departmentName, String headOfDepartmentName, LectorEntity lector) {
+		this.departmentName = departmentName;
+		this.headOfDepartmentName = headOfDepartmentName;
+		this.lector = lector;
+	}
+
+	public DepartmentEntity() {
+	}
+
+	public String getDepartmentName() {
+		return departmentName;
+	}
+
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+
+	public String getHeadOfDepartmentName() {
+		return headOfDepartmentName;
+	}
+
+	public void setHeadOfDepartmentName(String headOfDepartmentName) {
+		this.headOfDepartmentName = headOfDepartmentName;
+	}
+
+	public LectorEntity getLector() {
+		return lector;
+	}
+
+	public void setLector(LectorEntity lector) {
+		this.lector = lector;
+	}
+
+	@Override
+	public String toString() {
+		return "DepartmentEntity [departmentName=" + departmentName + ", headOfDepartmentName=" + headOfDepartmentName
+				+ ", lector=" + lector + "]";
+	}
 	
 }
--- src/main/java/com/botscrew/entity/LectorEntity.java
+++ src/main/java/com/botscrew/entity/LectorEntity.java
@@ -1,7 +1,11 @@
 package com.botscrew.entity;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -13,4 +17,46 @@ public class LectorEntity extends BaseEntity{
 	
 	@Column(name="degree")
 	private DegreeEnum degree;
+	
+	@OneToMany(cascade=CascadeType.ALL,mappedBy="lector")
+	private List<DepartmentEntity> departments;
+
+	public LectorEntity() {
+	}
+	
+	public LectorEntity(String lectorName, DegreeEnum degree, List<DepartmentEntity> departments) {
+		this.lectorName = lectorName;
+		this.degree = degree;
+		this.departments = departments;
+	}
+
+	public String getLectorName() {
+		return lectorName;
+	}
+
+	public void setLectorName(String lectorName) {
+		this.lectorName = lectorName;
+	}
+
+	public DegreeEnum getDegree() {
+		return degree;
+	}
+
+	public void setDegree(DegreeEnum degree) {
+		this.degree = degree;
+	}
+
+	public List<DepartmentEntity> getDepartments() {
+		return departments;
+	}
+
+	public void setDepartments(List<DepartmentEntity> departments) {
+		this.departments = departments;
+	}
+
+	@Override
+	public String toString() {
+		return "LectorEntity [lectorName=" + lectorName + ", degree=" + degree + ", departments=" + departments + "]";
+	}
+	
 }
